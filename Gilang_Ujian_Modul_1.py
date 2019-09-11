b1eec87ef9621c96c99e0a3f2acfeecc49666e01
Gilang Rizqinthara <54522198+Gilangthara@users.noreply.github.com>
2019-09-11 12:02:08 +0700

    Add files via upload

---
+++ Gilang_Ujian_Modul_1.py
@@ -0,0 +1,111 @@
+# Soal 1
+
+# import math
+# princpal = 1000.00
+# interest = 0.05
+# tax = 0.18
+# desire = 1100.00
+# sum_1 = []
+
+#  def calculate_years(principal, interest, tax, desire):
+#    if peincipal < 0:
+#       return 0
+#    else:    
+#       sum_m = []
+#       for i in range(1,principal+1):
+#           princ_y1 = ((principal*interest)-tax)
+#             i = princ_y1
+#          for j in range(i,i*2):
+#             if i == 1100.00 :
+#                sum_1.append(j)
+#             else:
+#                sum_1.append(j + sum_m[i*)
+#          sum_1.append(sum_m)
+#       sum_m = 0
+#       print(z)        
+#       return print('desire sum of {}'.format(sum_m))
+
+# calculate_years(principal, interest, tax, desire)
+
+# def calculate_years(principal, interest, tax, desire)
+# while(True):
+#    sum_m = []
+#    for i in calculate_years:
+#       d_year = (principal * interest)
+#       d_after_tax = (d_year) - tax
+#       sum_pr = d_after_tax
+#    return 3
+#    if sum_pr == 1100.00:
+#       sum_m.append(sum_pr)
+#          return 0
+#    elif sum_pr <= 1100.00:
+#       sum_pr += (0.05/0.18)
+#    print(sum_m)
+
+###-----------------------------------------------------     
+
+# # Soal 2
+# import math
+# def expandedForm(num):
+#    input_num = int(input("Masukan angka:"))
+#    hasil = []
+#    # hasil_s = input_num.split()
+#    for i in input_num:
+#       hasil.prepend(input_num)
+#       hasil.split()
+#       while(True):
+#       for i in range(len(hasil):
+#          if hasil < 0:
+#             print('coba lagi')
+#          elif hasil[0] >= 1 or <=9:
+#             hasil[0] * 100
+#                hasil[0] += 1
+#                hasil.append(i)
+#          elif hasil[1] >= 1 or <=9:
+#             hasil[1] * 10
+#                hasil[1] += 1
+#                hasil.append(i)
+#          elif hasil[2] >= 1 or <=9:
+#             hasil += 1
+#             hasil[2].append(i)
+#    for i in range(len(hasil)):
+#       print(input_num+" = "+str(hasil[0] "+" hasil[1] "+" hasil[2]))
+
+# expandedForm()
+
+
+###-----------------------------------------------------
+
+# Soal 3
+
+def w():
+   z= ''
+   for i in range (3,11,+3):
+      if i >= 10-i and i <= 10+i:
+            z += ' * '
+      else: 
+            z += '   '    
+ def h():
+    z= ''
+   for j in range (0,10,-3):
+      if j >= 10-i and j <= 10+i:
+            z += ' * '
+      else: 
+            z += '   '    
+   z += '\n'
+
+def tower_builder(n_floors, block_size):
+   w,h = block_size
+   
+   z= ''
+for i in range (0,10,1):
+    for j in range (0, 21):
+        if j >= 10-i and j <= 10+i:
+            z += ' * '
+        else: 
+            z += '   '    
+    z += '\n'
+print (z)  
+  
+
+  
